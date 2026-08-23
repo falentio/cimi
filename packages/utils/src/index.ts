60951dca205b2fce81c0e1fc1c12404875fb6366
falentio <kfalentio@proton.me>
2026-08-24 02:55:53 +0700

    feat(utils): add user-agent bot classification

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,2 +1,8 @@
 export { createSingleton } from './singleton/index.ts'
 export { ConfigError, loadConfig, type AppConfig } from './config/index.ts'
+export {
+  classifyUA,
+  isBotUA,
+  type BotCategory,
+  type BotClassification,
+} from './user-agent-bots/index.ts'
